f82996d2746091e1c884c93d6f8c128daefeb22c
Malay Agarwal <malay.agarwal261016@outlook.com>
2023-04-14 04:46:22 +0530

    Add initial logic for attacking enemies

--- yarl/actions.py
+++ yarl/actions.py
@@ -17,13 +17,28 @@ class EscapeAction(Action):
         raise SystemExit()
 
 
-class MovementAction(Action):
+class DirectedAction(Action):
     def __init__(self, dx: int, dy: int) -> None:
         super().__init__()
 
         self.dx = dx
         self.dy = dy
 
+
+class MeleeAction(DirectedAction):
+    def perform(self, engine: Engine, entity: Entity) -> None:
+        dest_x = entity.x + self.dx
+        dest_y = entity.y + self.dy
+
+        target = engine.game_map.get_blocking_entity(x=dest_x, y=dest_y)
+
+        if target is None:
+            return
+
+        print(f"You kick the {target.name}!")
+
+
+class MovementAction(DirectedAction):
     def perform(self, engine: Engine, entity: Entity) -> None:
         dest_x = entity.x + self.dx
         dest_y = entity.y + self.dy
@@ -41,3 +56,17 @@ class MovementAction(Action):
 
         game_map.update_entity_location(entity=entity, x=dest_x, y=dest_y)
         entity.move(dx=self.dx, dy=self.dy)
+
+
+class BumpAction(DirectedAction):
+    def perform(self, engine: Engine, entity: Entity) -> None:
+        dest_x = entity.x + self.dx
+        dest_y = entity.y + self.dy
+
+        action = (
+            MeleeAction(dx=self.dx, dy=self.dy)
+            if engine.game_map.get_blocking_entity(dest_x, dest_y) is not None
+            else MovementAction(dx=self.dx, dy=self.dy)
+        )
+
+        action.perform(engine, entity)
