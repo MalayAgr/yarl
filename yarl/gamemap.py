137e9165202ca1cc945ec998008830ba705caf2c
Malay Agarwal <malay.agarwal261016@outlook.com>
2023-04-14 00:10:19 +0530

    Add method to GameMap to add entites

--- yarl/gamemap.py
+++ yarl/gamemap.py
@@ -1,23 +1,32 @@
 from __future__ import annotations
 
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, Iterable
 
 import numpy as np
 import tcod
 import yarl.tile_types as tiles
 from tcod.console import Console
 from tcod.map import compute_fov
+from yarl.exceptions import CollisionWithEntityException
 
 if TYPE_CHECKING:
     from yarl.entity import Entity
 
 
 class GameMap:
-    def __init__(self, width: int, height: int, pov_radius: int = 5):
+    def __init__(
+        self,
+        width: int,
+        height: int,
+        pov_radius: int = 5,
+        entities: Iterable[Entity] = (),
+    ):
         self.width, self.height = width, height
         self.pov_radius = pov_radius
-        self.tiles = np.full((width, height), fill_value=tiles.wall, order="F")
+        self.entities = set(entities)
+        self._entity_map = {(entity.x, entity.y): entity for entity in entities}
 
+        self.tiles = np.full((width, height), fill_value=tiles.wall, order="F")
         self.visible = np.full((width, height), fill_value=False, order="F")
         self.explored = np.full((width, height), fill_value=False, order="F")
 
@@ -34,9 +43,31 @@ class GameMap:
         )
         self.explored |= self.visible
 
+    def get_blocking_entity(self, x: int, y: int) -> Entity | None:
+        return self._entity_map.get((x, y), None)
+
+    def add_entity(self, entity: Entity, location: tuple[int, int] = (-1, -1)) -> None:
+        x, y = location
+
+        x = x if x != -1 else entity.x
+        y = y if y != -1 else entity.y
+
+        if self.get_blocking_entity(x, y) is not None:
+            raise CollisionWithEntityException(f"An entity already exists at ({x}, {y})")
+
+        entity.place(x=x, y=y)
+        self.entities.add(entity)
+        self._entity_map[(x, y)] = entity
+
     def render(self, console: Console) -> None:
         console.tiles_rgb[0 : self.width, 0 : self.height] = np.select(
             condlist=[self.visible, self.explored],
             choicelist=[self.tiles["light"], self.tiles["dark"]],
             default=tiles.SHROUD,
         )
+
+        for entity in self.entities:
+            if self.visible[entity.x, entity.y]:
+                console.print(
+                    x=entity.x, y=entity.y, string=entity.char, fg=entity.color
+                )
