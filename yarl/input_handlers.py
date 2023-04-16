121d36a1d0a40ce9a3e5aa78daab35dd6d1e9954
Malay Agarwal <malay.agarwal261016@outlook.com>
2023-04-16 21:53:41 +0530

    Add method to modify entity state upon death and add more key bindings

--- yarl/input_handlers.py
+++ yarl/input_handlers.py
@@ -2,14 +2,33 @@ from __future__ import annotations
 
 import tcod.event
 from tcod.event import KeySym
-from yarl.actions import Action, BumpAction, EscapeAction, MovementAction
+from yarl.actions import Action, BumpAction, EscapeAction, MovementAction, WaitAction
 
-KEY_MAP: dict[KeySym, Action] = {
-    tcod.event.K_UP: BumpAction(dx=0, dy=-1),
-    tcod.event.K_DOWN: BumpAction(dx=0, dy=1),
-    tcod.event.K_LEFT: BumpAction(dx=-1, dy=0),
-    tcod.event.K_RIGHT: BumpAction(dx=1, dy=0),
+MOVE_KEYS: dict[KeySym, Action] = {
+    # Character keys
+    tcod.event.K_w: BumpAction(dx=0, dy=-1),
+    tcod.event.K_s: BumpAction(dx=0, dy=1),
+    tcod.event.K_a: BumpAction(dx=-1, dy=0),
+    tcod.event.K_d: BumpAction(dx=1, dy=0),
+    tcod.event.K_q: BumpAction(dx=-1, dy=-1),
+    tcod.event.K_e: BumpAction(dx=1, dy=-1),
+    tcod.event.K_c: BumpAction(dx=1, dy=1),
+    tcod.event.K_z: BumpAction(dx=-1, dy=1),
+    # Numpad keys
+    tcod.event.K_KP_8: BumpAction(dx=0, dy=-1),
+    tcod.event.K_KP_2: BumpAction(dx=0, dy=1),
+    tcod.event.K_KP_4: BumpAction(dx=-1, dy=0),
+    tcod.event.K_KP_6: BumpAction(dx=1, dy=0),
+    tcod.event.K_KP_7: BumpAction(dx=-1, dy=-1),
+    tcod.event.K_KP_9: BumpAction(dx=1, dy=-1),
+    tcod.event.K_KP_3: BumpAction(dx=1, dy=1),
+    tcod.event.K_KP_1: BumpAction(dx=-1, dy=1),
     tcod.event.K_ESCAPE: EscapeAction(),
+}
+
+
+WAIT_KEYS: dict[KeySym, Action] = {
+    tcod.event.K_KP_5: WaitAction()
 }
 
 
@@ -20,4 +39,8 @@ class EventHandler(tcod.event.EventDispatch[Action]):
     def ev_keydown(self, event: tcod.event.KeyDown) -> Action | None:
         key = event.sym
 
-        return KEY_MAP.get(key)
+        if key in MOVE_KEYS:
+            return MOVE_KEYS.get(key)
+
+        if key in WAIT_KEYS:
+            return WAIT_KEYS.get(key)
--- yarl/utils/fighter.py
+++ yarl/utils/fighter.py
@@ -2,12 +2,15 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING
 
+
 if TYPE_CHECKING:
-    from yarl.entity import Entity
+    from yarl.entity import ActiveEntity
 
 
 class Fighter:
-    def __init__(self, entity: Entity, max_hp: int, defense: int, power: int) -> None:
+    def __init__(
+        self, entity: ActiveEntity, max_hp: int, defense: int, power: int
+    ) -> None:
         self.entity = entity
         self.max_hp = max_hp
         self._hp = max_hp
@@ -21,3 +24,17 @@ class Fighter:
     @hp.setter
     def hp(self, hp: int) -> None:
         self._hp = max(0, min(hp, self.max_hp))
+
+        if self._hp <= 0:
+            self.die()
+
+    @property
+    def damage(self) -> int:
+        return self.power - self.defense
+
+    def die(self) -> None:
+        self.entity.char = "%"
+        self.entity.color = (191, 0, 0)
+        self.entity.blocks_movement = False
+        self.entity.ai_cls = None
+        self.entity.name = f"remains of {self.entity.name}"
