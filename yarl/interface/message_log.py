aa1a1f0c838f54e1ef451ae4d83f77bacf224c4e
Malay Agarwal <malay.agarwal261016@outlook.com>
2023-04-17 05:03:30 +0530

    Use MessageLog in Engine to render messages

---
+++ yarl/interface/message_log.py
@@ -0,0 +1,63 @@
+from __future__ import annotations
+
+import textwrap
+from typing import Reversible
+
+import tcod
+from yarl.interface import color
+
+
+class Message:
+    def __init__(self, text: str, fg: tuple[int, int, int]) -> None:
+        self.plain_text = text
+        self.fg = fg
+        self.count = 1
+
+    @property
+    def full_text(self) -> str:
+        return (
+            f"{self.plain_text} (x{self.count})" if self.count > 1 else self.plain_text
+        )
+
+
+class MessageLog:
+    def __init__(self) -> None:
+        self.messages: list[Message] = []
+
+    def add_message(
+        self, text: str, fg: tuple[int, int, int] = color.WHITE, *, stack: bool = True
+    ) -> Message:
+        if stack and self.messages and self.messages[-1].plain_text == text:
+            self.messages[-1].count += 1
+            return
+
+        self.messages.append(Message(text=text, fg=fg))
+
+    def render(self, console: tcod.Console, x: int, y: int, width: int, height: int):
+        render_messages(
+            console=console,
+            x=x,
+            y=y,
+            width=width,
+            height=height,
+            messages=self.messages,
+        )
+
+
+def render_messages(
+    console: tcod.Console,
+    x: int,
+    y: int,
+    width: int,
+    height: int,
+    messages: Reversible[Message],
+) -> None:
+    for message in reversed(messages):
+        lines = reversed(textwrap.wrap(text=message.full_text, width=width))
+
+        for line in lines:
+            console.print(x=x, y=y + height - 1, string=line, fg=message.fg)
+            height -= 1
+
+            if height <= 0:
+                return
