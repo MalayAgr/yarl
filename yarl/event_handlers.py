9eeee595d1f88d18ca74429fe988f77e620069fe
Malay Agarwal <malay.agarwal261016@outlook.com>
2023-04-17 22:34:22 +0530

    Add event handler for viewing message log history

--- yarl/event_handlers.py
+++ yarl/event_handlers.py
@@ -43,19 +43,12 @@ MOVE_KEYS: dict[KeySym, Deviation] = {
 
 WAIT_KEYS: set[KeySym] = {tcod.event.K_KP_5}
 
-
-def get_action(key: KeySym, engine: Engine, entity: Entity) -> Action | None:
-    if key == tcod.event.K_ESCAPE:
-        return EscapeAction(engine=engine, entity=entity)
-
-    if key in MOVE_KEYS:
-        deviation = MOVE_KEYS.get(key)
-        return BumpAction(
-            engine=engine, entity=entity, dx=deviation.dx, dy=deviation.dy
-        )
-
-    if key in WAIT_KEYS:
-        return WaitAction(engine=engine, entity=entity)
+CURSOR_Y_KEYS = {
+    tcod.event.K_UP: -1,
+    tcod.event.K_DOWN: 1,
+    tcod.event.K_PAGEUP: -10,
+    tcod.event.K_PAGEDOWN: 10,
+}
 
 
 class EventHandler(tcod.event.EventDispatch[Action]):
@@ -117,10 +110,29 @@ class MainGameEventHandler(EventHandler):
             self.handle_enemy_turns()
             self.engine.update_fov()
 
+    def get_action(self, key: KeySym) -> Action | None:
+        engine, entity = self.engine, self.engine.player
+
+        if key == tcod.event.K_ESCAPE:
+            return EscapeAction(engine=engine, entity=entity)
+
+        if key == tcod.event.K_v:
+            engine.event_handler = HistoryEventHandler(engine=engine)
+            return
+
+        if key in MOVE_KEYS:
+            deviation = MOVE_KEYS.get(key)
+            return BumpAction(
+                engine=engine, entity=entity, dx=deviation.dx, dy=deviation.dy
+            )
+
+        if key in WAIT_KEYS:
+            return WaitAction(engine=engine, entity=entity)
+
     def ev_keydown(self, event: tcod.event.KeyDown) -> Action | None:
         key = event.sym
 
-        return get_action(key=key, engine=self.engine, entity=self.engine.player)
+        return self.get_action(key=key)
 
 
 class GameOverEventHandler(EventHandler):
@@ -145,3 +157,58 @@ class GameOverEventHandler(EventHandler):
 
         # No valid key was pressed
         return action
+
+
+class HistoryEventHandler(EventHandler):
+    """Print the history on a larger window which can be navigated."""
+
+    def __init__(self, engine: Engine):
+        super().__init__(engine)
+        self.log_length = len(engine.message_log.messages)
+        self.cursor = self.log_length - 1
+
+    def on_render(self, console: tcod.Console) -> None:
+        super().on_render(console)  # Draw the main state as the background.
+
+        log_console = tcod.Console(console.width - 6, console.height - 6)
+
+        # Draw a frame with a custom banner title.
+        log_console.draw_frame(0, 0, log_console.width, log_console.height)
+        log_console.print_box(
+            0, 0, log_console.width, 1, "┤Message history├", alignment=tcod.CENTER
+        )
+
+        # Render the message log using the cursor parameter.
+        self.engine.message_log.render_messages(
+            console=log_console,
+            x=1,
+            y=1,
+            width=log_console.width - 2,
+            height=log_console.height - 2,
+            messages=self.engine.message_log.messages[: self.cursor + 1],
+        )
+
+        log_console.blit(console, 3, 3)
+
+    def ev_keydown(self, event: tcod.event.KeyDown) -> None:
+        # Fancy conditional movement to make it feel right.
+        if event.sym in CURSOR_Y_KEYS:
+            adjust = CURSOR_Y_KEYS[event.sym]
+            if adjust < 0 and self.cursor == 0:
+                # Only move from the top to the bottom when you're on the edge.
+                self.cursor = self.log_length - 1
+            elif adjust > 0 and self.cursor == self.log_length - 1:
+                # Same with bottom to top movement.
+                self.cursor = 0
+            else:
+                # Otherwise move while staying clamped to the bounds of the history log.
+                self.cursor = max(0, min(self.cursor + adjust, self.log_length - 1))
+
+        elif event.sym == tcod.event.K_HOME:
+            self.cursor = 0  # Move directly to the top message.
+
+        elif event.sym == tcod.event.K_END:
+            self.cursor = self.log_length - 1  # Move directly to the last message.
+
+        else:  # Any other key moves back to the main game state.
+            self.engine.event_handler = MainGameEventHandler(self.engine)
--- yarl/interface/message_log.py
+++ yarl/interface/message_log.py
@@ -34,7 +34,7 @@ class MessageLog:
         self.messages.append(Message(text=text, fg=fg))
 
     def render(self, console: tcod.Console, x: int, y: int, width: int, height: int):
-        render_messages(
+        self.render_messages(
             console=console,
             x=x,
             y=y,
@@ -43,21 +43,21 @@ class MessageLog:
             messages=self.messages,
         )
 
+    @staticmethod
+    def render_messages(
+        console: tcod.Console,
+        x: int,
+        y: int,
+        width: int,
+        height: int,
+        messages: Reversible[Message],
+    ) -> None:
+        for message in reversed(messages):
+            lines = reversed(textwrap.wrap(text=message.full_text, width=width))
 
-def render_messages(
-    console: tcod.Console,
-    x: int,
-    y: int,
-    width: int,
-    height: int,
-    messages: Reversible[Message],
-) -> None:
-    for message in reversed(messages):
-        lines = reversed(textwrap.wrap(text=message.full_text, width=width))
+            for line in lines:
+                console.print(x=x, y=y + height - 1, string=line, fg=message.fg)
+                height -= 1
 
-        for line in lines:
-            console.print(x=x, y=y + height - 1, string=line, fg=message.fg)
-            height -= 1
-
-            if height <= 0:
-                return
+                if height <= 0:
+                    return
