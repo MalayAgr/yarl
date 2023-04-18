e286264d73c757406685e29e2f38ce4ff1b06535
Malay Agarwal <malay.agarwal261016@outlook.com>
2023-04-18 10:46:39 +0530

    Add initial classes for consumables

---
+++ yarl/utils/consumable.py
@@ -0,0 +1,23 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from yarl.entity import ActiveEntity, Item
+
+
+class Consumable:
+    def __init__(self, item: Item):
+        self.item = item
+
+    def activate(self, consumer: ActiveEntity) -> None:
+        raise NotImplementedError()
+
+
+class HealingPotion(Consumable):
+    def __init__(self, amount: int) -> None:
+        self.amount = amount
+
+    def activate(self, consumer: ActiveEntity) -> None:
+        recovered = consumer.fighter.heal(amount=self.amount)
+        return recovered
