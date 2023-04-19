957ed5c1c788e733207d2050ad68e84c36aeedb2
Malay Agarwal <malay.agarwal261016@outlook.com>
2023-04-19 23:48:23 +0530

    Add initial logic to consume items

--- yarl/utils/consumable.py
+++ yarl/utils/consumable.py
@@ -2,7 +2,11 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING
 
+from yarl.exceptions import ImpossibleActionException
+from yarl.interface import color
+
 if TYPE_CHECKING:
+    from yarl.engine import Engine
     from yarl.entity import ActiveEntity, Item
 
 
@@ -15,9 +19,15 @@ class Consumable:
 
 
 class HealingPotion(Consumable):
-    def __init__(self, amount: int) -> None:
+    def __init__(self, item: Item, amount: int):
+        super().__init__(item=item)
         self.amount = amount
 
-    def activate(self, consumer: ActiveEntity) -> None:
+    def activate(self, consumer: ActiveEntity, engine: Engine) -> None:
         recovered = consumer.fighter.heal(amount=self.amount)
-        return recovered
+
+        if recovered > 0:
+            text = f"You consume the {self.item.name}, and recover {recovered} amount of HP!"
+            engine.add_to_message_log(text=text, fg=color.HEALTH_RECOVERED)
+        else:
+            raise ImpossibleActionException("Your health is already full.")
