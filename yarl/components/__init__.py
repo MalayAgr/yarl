7acd32241f190edfca85482abcbd7b0a0ef64273
Malay Agarwal <malay.agarwal261016@outlook.com>
2023-05-08 21:19:38 +0530

    Revert "Fix type hints"
    This reverts commit b0d4fd8326bb2d6a788190278f2accdbf05ef9be.

--- yarl/components/__init__.py
+++ yarl/components/__init__.py
@@ -6,7 +6,6 @@ from .consumable import (
     HealingPotion,
     LightningScroll,
 )
-from .equippable import Equippable
 from .fighter import Fighter
 from .inventory import Inventory
 from .level import Level
--- yarl/components/equippable.py
+++
@@ -1,7 +0,0 @@
-from yarl.entity import EquippableItem
-
-from .base_component import Component
-
-
-class Equippable(Component[EquippableItem]):
-    pass
